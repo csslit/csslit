6152091af866395b734feb9593ba5eaf2fc487d2
Gabriel Valfridsson <gabriel.valfridsson@gmail.com>
2026-03-22 23:37:26 +0100

    add vite-plugin-inspect for dev-mode transform debugging

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { cssCompilePlugin } from '@csslit/vite-plugin';
+import Inspect from 'vite-plugin-inspect';
 
 export default defineConfig({
   plugins: [
     cssCompilePlugin(),
-    react()
-  ]
+    react(),
+    Inspect()
+  ],
+  build: {
+    minify: false,
+    cssMinify: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 });
